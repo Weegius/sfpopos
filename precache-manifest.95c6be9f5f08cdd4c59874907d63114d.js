673b114b774200b195d3558792efb93a27a13b4f
Christian Thompson <ChristianThompson1@hotmail.com>
2022-02-11 16:21:47 -0800

    Updates

---
+++ precache-manifest.95c6be9f5f08cdd4c59874907d63114d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ac993849c71c8b755d3619be17098e",
+    "url": "/Weegius/sfpopops.git/index.html"
+  },
+  {
+    "revision": "c8aa40993529f5baf7af",
+    "url": "/Weegius/sfpopops.git/static/css/main.ea140e1c.chunk.css"
+  },
+  {
+    "revision": "ddbd1577d24baca8704b",
+    "url": "/Weegius/sfpopops.git/static/js/2.c2c94f97.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/Weegius/sfpopops.git/static/js/2.c2c94f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8908b4636ac053536c3f",
+    "url": "/Weegius/sfpopops.git/static/js/3.35ed35bd.chunk.js"
+  },
+  {
+    "revision": "c8aa40993529f5baf7af",
+    "url": "/Weegius/sfpopops.git/static/js/main.74a295b5.chunk.js"
+  },
+  {
+    "revision": "b23c23425eddc11f6221",
+    "url": "/Weegius/sfpopops.git/static/js/runtime-main.1cb15de4.js"
+  }
+]);
